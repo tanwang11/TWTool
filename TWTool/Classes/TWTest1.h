789dee07ad682134d0b1adb1d742ababae1c708c
艾慧勇 <aihy@linrunwc.com>
2021-03-04 13:14:40 +0800

    1

--- TWTool/Classes/TWTest1.h
+++ TWTool/Classes/TWTest1.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface TWTest1 : NSObject
 
++ (void)testLog;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- TWTool/Classes/TWTool.h
+++ TWTool/Classes/TWTool.h
@@ -1,18 +1,16 @@
 //
-//  TWTest1.m
+//  TWTool.h
 //  TWTool_Example
 //
 //  Created by TW on 2021/3/4.
 //  Copyright © 2021 tanwang11. All rights reserved.
 //
 
+#ifndef TWTool_h
+#define TWTool_h
+
 #import "TWTest1.h"
 
-@implementation TWTest1
-
-+ (void)testLog {
-    NSLog(@"TWTool testLog");
-}
 
 
-@end
+#endif /* TWTool_h */
